a03b9c83420ba604e3e5cdd5892ef7c076a0b4ac
codeuniquecode <uniquestha619@gmail.com>
2023-12-14 16:03:17 +0545

    array operations in js

---
+++ arraj.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3,4,5];
+console.log(arr.join("&"));
+console.log(arr.toString());
+console.log('push opration')
+console.log(arr.push("push"))
+console.log(arr)
+console.log('pop opeariaon')
+console.log(arr.pop())
+console.log(arr)
+console.log("shift operation")
+console.log(arr.shift("shift"))
+console.log(arr)
+console.log("unshift opration")
+console.log(arr.unshift())
+console.log(arr)
